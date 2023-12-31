00cd0a2307e5e9831796a140e440ee581252b96c
rodoolfogonzalez <rodolfo.swift.dev@gmail.com>
2023-12-31 11:15:37 -0300

    Initial Commit

---
+++ tesUiComponents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tesUiComponents
+//
+//  Created by rodoolfo gonzalez on 31-12-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
